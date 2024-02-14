c1eaa9979f84ee42668d15f41ded3d167f632d8a
rigobertopo <rperez@cobach.edu.mx>
2024-02-14 17:23:33 -0600

    Agregar archivos de proyecto.

---
+++ UNACH.Demo1.CalculadoraWF/Calcular.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNACH.Demo1.CalculadoraWF
+{
+    class Calcular
+    {
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="valor1"></param>
+        /// <param name="valor2"></param>
+        /// <returns></returns>
+        public int Sumar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+        public int Restar(int valor1, int valor2)
+        {
+            int resultado = valor1 - valor2;
+            return resultado;
+        }
+        public int Multiplicar()
+        {
+            return Numero1 * Numero2;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public float Dividir()
+        {
+            float resultado = Numero1 / Numero2;
+            return resultado;
+        }
+    }
+}
---
+++ UNACH.Demo1.CalculadoraWF/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UNACH.Demo1.CalculadoraWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            Calcular calcular = new Calcular();
+            int resultado =
+                calcular.
+                Sumar(int.Parse(txtNumero1.Text),
+                Convert.ToInt32(txtNumero2.Text));
+            lblResultado.Text = 
+                resultado.ToString();
+        }
+    }
+}
